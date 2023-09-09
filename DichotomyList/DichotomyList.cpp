a93e8740182a9cc5971390b6d55eefc450175e33
ricardo <gq1090523745@163.com>
2023-09-09 21:45:45 +0800

    排序等算法

--- DichotomyList/DichotomyList.cpp
+++ DichotomyList/DichotomyList.cpp
@@ -29,9 +29,9 @@ void DichotomyList::Test_LC162_findPeakElement(){
     vector<int>nums={1,2,1,2,1};
     cout<<LC162_findPeakElement_(nums)<<endl;
 }
+
 /// @brief 快速排序
 /// @param nums 
-
 void DichotomyList::QuickSort_(vector<int>&nums){
     QuickSort_Switch2(nums,0,nums.size()-1);
 }
@@ -110,22 +110,28 @@ void DichotomyList::Swap(vector<int>&nums,int left,int right){
     nums[left]=nums[right];
     nums[right]=temp;
 }
+void DichotomyList::Swap_Beta(vector<int>&nums,int left,int right){
+    if(left^right==0)return;
+    //相同内存的数字异或结果会保持为0;
+    nums[left]=nums[left]^nums[right];
+    nums[right]=nums[right]^nums[left];//(a^b)^b=a^(b^b)=a^0=a
+    nums[left]=nums[left]^nums[right];//(a^b)^a=(a^a)^b=0^b=b
+}
 int DichotomyList::SelectMidNum_(int left,int right,int mid,vector<int>&nums){
     if(nums[left]<nums[mid]^nums[left]<nums[right])return left;
     else if(nums[mid]<=nums[left]^nums[mid]<nums[right])return mid;
     else return right;
 }
-
 void DichotomyList::Test_QuickSort(){
     Cleanup cle;
     for(int i=0;i<1;i++){
         //vector<int>nums={0,1,0,1,2,5};
-        vector<int>nums=cle.RandVector(50000,2,2,i);
+        vector<int>nums=cle.RandVector(10,-10,10,i);
         vector<int>num1=nums;
         vector<int>num2=nums;
         cout<<"原数组为："<<endl;
         //cle.Print(nums);
-        QuickSort_(num1);
+        Swap_Beta(nums,3,3);
         //cle.Print(num1);
         //Sortingalgorithms_(num2);
         //cle.Print(num2);
@@ -133,6 +139,7 @@ void DichotomyList::Test_QuickSort(){
         //else cout<<"false!"<<endl;
     }
 }
+
 /// @brief 冒泡排序
 /// @param nums 
 void DichotomyList::Sortingalgorithms_(vector<int>&nums){
@@ -142,6 +149,7 @@ void DichotomyList::Sortingalgorithms_(vector<int>&nums){
         }
     }
 }
+
 /// @brief 归并排序
 /// @param nums 
 void DichotomyList::Merge_Sort_(vector<int>&nums){
@@ -179,6 +187,7 @@ void DichotomyList::Test_Merge_Sort(){
         //else cout<<"False!"<<endl;
     }
 }
+
 /// @brief 堆排序
 /// @param nums 
 /// @return 
@@ -214,12 +223,12 @@ void DichotomyList::adjustHeap_(vector<int>&nums,int i,int n){
 void DichotomyList::Test_SortArray(){
     Cleanup cle;
     for(int i=0;i<1;i++){
-        vector<int>nums=cle.RandVector(50000,2,2,i);
+        vector<int>nums=cle.RandVector(1,2,2,i);
         vector<int>num1=nums;
         vector<int>num2=nums;
         //cle.Print(nums);
         SortArray_(num1);
-        //cle.Print(num1);
+        cle.Print(num1);
         //Sortingalgorithms_(num2);
         //cle.Print(num2);
         //if(cle.LogarithmicAxis(num1,num2))cout<<"True!"<<endl;
@@ -227,6 +236,9 @@ void DichotomyList::Test_SortArray(){
     }
 }
 
+/// @brief 希尔排序
+/// @param nums 
+/// @return 
 vector<int>DichotomyList::HearArrySort_(vector<int>&nums){
     //设置希尔增量
     int n=nums.size();
@@ -265,12 +277,285 @@ void DichotomyList::Test_HearArrySort_(){
     }
 }
 
+/// @brief 最多重合的线段
+/// @param n1 
+/// @param n2 
+/// @return 
+bool cmp(vector<int>&n1,vector<int>&n2){
+    return n1[0]<n2[0];
+}
+int DichotomyList::TheMostCoincidentSegments_(vector<vector<int>>&line){
+    int ans=0;
+    sort(line.begin(),line.end(),cmp);
+    priority_queue<int,vector<int>,greater<int>>q;
+    for(vector<int>l:line){
+        while(!q.empty()&&q.top()<=l[0]){
+            q.pop();
+        }
+        q.push(l[1]);
+        ans=max(ans,(int)q.size());
+    }
+    return ans;
+}
+void DichotomyList::Test_TheMostCoincidentSegments(){
+    //vector<vector<int>>nums={{2,6},{3,7},{5,9},{1,5},{2,4},{1,3}};
+    //cout<<TheMostCoincidentSegments_(nums)<<endl;
+    Cleanup cle;
+    vector<vector<int>>nums;
+    for(int i=0;i<5000;i++){
+        nums.push_back(cle.RandVectorDifferent(2,-100,100,i));
+        if(nums[i][0]>nums[i][1])Swap(nums[i],0,1);
+        //cle.Print(nums[i]);
+    }
+    cout<<TheMostCoincidentSegments_(nums)<<endl;
+}
+
+/// @brief LeetCode 2208 将数组和减半的最少操作次数
+/// @param nums 
+/// @return 
+int DichotomyList::LC2208_halveArray_(std::vector<int>&nums){
+    priority_queue<double,vector<double>,less<double>>que;
+    double sum=0;
+    for(int n:nums){
+        que.push((double)n);
+        sum+=n;
+    }
+    double beg_sum=sum/2;
+    int ans=0;
+    while(!que.empty()&&sum>=beg_sum){
+        sum-=que.top()/2;
+        que.push(que.top()/2);
+        que.pop();
+        ans++;
+        if(sum==beg_sum)break;
+        
+    }
+    return ans;
+}
+void DichotomyList::Test_LC2208_halveArray(){
+    //vector<int>nums={5,19,8,1};
+    //LC2208_halveArray_(nums);
+    Cleanup cle;
+    for(int i=0;i<10;i++){
+        vector<int>nums=cle.RandVector(10,0,20,i);
+        cle.Print(nums);
+        cout<<LC2208_halveArray_(nums)<<endl;
+    }
+}
+
+/// @brief 不使用比较而判断两个值中的最大值
+/// @param a 
+/// @param b 
+/// @return 
+int DichotomyList::GetMax(int a,int b){
+    int c=a-b;
+    int sa=sign(a);
+    int sb=sign(b);
+    int sc=sign(c);
+    //判断a与b的符号是否一样
+    int diffAB=sa^sb;
+    //一样的话
+    int sameAB=flip(diffAB);
+    //如果符号不一样且a是非负的或者符号一样且c是非负的都返回a为最大值
+    int returnA=diffAB*sa+sameAB*sc;
+    //反之返回b
+    int returnB=flip(returnA);
+
+    return a*returnA+b*returnB;
+}
+int DichotomyList::flip(int sign){
+    return sign^1;
+}
+int DichotomyList::sign(int num){
+    return flip((num>>31)&1);
+}
+void DichotomyList::Test_GetMax(){
+    Cleanup cle;
+    for(int i=0;i<10;i++){
+        vector<int>nums=cle.RandVectorDifferent(2,-100,100,i);
+        cle.Print(nums);
+        cout<<GetMax(nums[0],nums[1])<<endl;
+    }
+}
+
+/// @brief LeetCode 260 只出现奇数次的数字
+/// @param nums 
+/// @return 
+vector<int>DichotomyList::LC260_singleNumber(vector<int>&nums){
+    int eor1=0,eor2=0;
+    for(int n:nums)eor1^=n;
+    //此时eor就是两个出现奇数次的数的异或值
+    //所以eor最右侧的1必为两个数中的一个的1
+    int DataOne=eor1&(-eor1);
+    //通过与操作将数组中的数分为最右侧右这个1的与没有的
+    for(int n:nums){
+        if((n&DataOne)==0){//如果这个数没有这个1
+            eor2^=n;//找出这部分出现奇数次的数
+        }
+    }
+    return {eor2,eor1^eor2};
+}
+void DichotomyList::Test_LC260_singleNumber(){
+    vector<int>nums={1,2,1,3,2,5};
+    vector<int>ans=LC260_singleNumber(nums);
+    cout<<ans[0]<<" "<<ans[1]<<endl;
+}
+
+/// @brief 判断一个数是否是2的幂
+/// @param n 
+/// @return 
+bool DichotomyList::isPowerOfTwo(int n){
+    return n>0&&n==(n&-n);
+}
+void DichotomyList::Test_isPowerOfTwo(){
+    Cleanup cle;
+    vector<int>nums=cle.RandVector(10,0,50000,10);
+    for(int i=0;i<10;i++){
+        cout<<nums[i]<<endl;
+        if(isPowerOfTwo(nums[i]))cout<<"True!";
+        else cout<<"False!";
+
+    }
+}
+
+/// @brief 返回大于等于n的最小的2的某次方
+/// @param n 
+/// @return 
+int DichotomyList::near2power(int n){
+    if(n<0)return 0;
+    n--;
+    n|=n>>1;
+    n|=n>>2;
+    n|=n>>4;
+    n|=n>>8;
+    n|=n>>16;
+    return n+1;
+}
+void DichotomyList::near2power(){
+    Cleanup cle;
+    vector<int>nums=cle.RandVector(20,0,996,10);
+    for(int i:nums){
+        cout<<i<<" "<<near2power(i)<<endl;
+    }
+}
+
+/// @brief LeetCode 190 颠倒二进制位
+/// @param n 
+/// @return 
+uint32_t DichotomyList::LC190_reverseBits(uint32_t n){
+    //1010 1010=0xaa 
+    //0101 0101=0x55
+    n=((n&0xaaaaaaaa)>>1|(n&0x55555555)<<1);
+    //1100 1100=0xcc
+    //0011 0011=0x33
+    n=((n&0xcccccccc)>>2|(n&0x33333333)<<2);
+    //11110000 11110000=0xf0f0
+    //00001111 00001111=0x0f0f
+    n=((n&0xf0f0f0f0)>>4|(n&0x0f0f0f0f)<<4);
+    //11111111 00000000=0xff00
+    //00000000 11111111=0x00ff
+    n=((n&0xff00ff00)>>8|(n&0x00ff00ff)<<8);
+    n=(n>>16)|(n<<16);
+    return n;
+}
+void DichotomyList::Test_LC190_reverseBits(){
+    Cleanup cle;
+    vector<int>nums=cle.RandVector(10,0,100,10);
+    for(int i=0;i<10;i++){
+        uint32_t num=(uint32_t)nums[i];
+        cout<<(bitset<sizeof(num)*8>)num<<endl;
+        cout<<(bitset<sizeof(num)*8>)LC190_reverseBits(num)<<endl;
+    }
+}
+
+/// @brief 位图对数器
+/// @param nums 
+/// @param MaxNum 
+/// @return 
+bool DichotomyList::BitSetComp(vector<int>&nums,int MaxNum){
+    bool flag=true;
+    BitSet bit(MaxNum);
+    unordered_map<int,int>map;
+    Cleanup cle;
+    vector<double>decied=cle.RandVector(10000,(double)0,(double)1,10);
+    cout<<"调用阶段开始"<<endl;
+    for(int i=0;i<decied.size();i++){
+        if(decied[i]<0.333){
+            map[nums[i]]++;
+            bit.BitSetAdd(nums[i]);
+        }else if(decied[i]<0.666){
+            map.erase(nums[i]);
+            bit.BitSetRemove(nums[i]);
+        }else{
+            if(map.count(nums[i]))map.erase(nums[i]);
+            else map[nums[i]]++;
+            bit.BitSetReverse(nums[i]);
+        }
+    }
+    cout<<"调用阶段结束"<<endl;
+    cout<<"验证阶段开始"<<endl;
+    for(int i=0;i<MaxNum;i++){
+        if(bit.BitSetContains(i)!=map.count(i)){
+            cout<<"出错了"<<endl;
+            flag=false;
+        }
+    }
+    cout<<"验证结束！"<<endl;
+    return true;
+}
+void DichotomyList::Test_BitSetComp(){
+    Cleanup cle;
+    vector<int>nums=cle.RandVector(10000,0,1000,10);
+    if(BitSetComp(nums,1000))cout<<"True"<<endl;
+    else cout<<"False!"<<endl;
+}
+
+/// @brief 位运算对数器
+/// @param num1 
+/// @param num2 
+void DichotomyList::BitOperator_Cop(vector<int>num1,vector<int>num2){
+    vector<int>ans1(num1.size());
+    vector<int>ans2(num2.size());
+    Cleanup cle;
+    vector<double>decied=cle.RandVector(100000,0.0,1.0,10);
+    BitOperator Bpr;
+    cout<<"开始调用"<<endl;
+    for(int i=0;i<100000;i++){
+        if(decied[i]<0.25){
+            ans1[i]=Bpr.BitAdd(num1[i],num2[i]);
+            ans2[i]=num1[i]+num2[i];
+        }else if(decied[i]<0.5){
+            ans1[i]=Bpr.BitMinus(num1[i],num2[i]);
+            ans2[i]=num1[i]-num2[i];
+        }else if(decied[i]<0.75){
+            ans1[i]=Bpr.BitMultiply(num1[i],num2[i]);
+            ans2[i]=num1[i]*num2[i];
+        }else{
+            if(num2[i]==0)continue;
+            ans1[i]=Bpr.BitDived(num1[i],num2[i]);
+            ans2[i]=num1[i]/num2[i];
+        }
+    }
+    cout<<"调用阶段结束"<<endl;
+    cout<<"开始测试"<<endl;
+    for(int i=0;i<ans1.size();i++){
+        if(ans1[i]!=ans2[i])cout<<"答案错误!"<<endl;
+    }
+    cout<<"测试结束"<<endl;
+
+}
+void DichotomyList::Test_BitOperator_Cop(){
+    Cleanup cle;
+    vector<int>num1=cle.RandVector(100000,-100000,100000,10);
+    vector<int>num2=cle.RandVector(100000,-999999,999999,100);
+    BitOperator_Cop(num1,num2);
+}
 
 int main(int argc,char*argv[]){
     chrono::_V2::system_clock::time_point start=chrono::system_clock::now();
 
     DichotomyList dic;
-    dic.Test_HearArrySort_();
+    dic.Test_BitOperator_Cop();
 
     chrono::_V2::system_clock::time_point end=chrono::system_clock::now();
     auto duration=chrono::duration_cast<chrono::milliseconds>(end-start);
--- DichotomyList/DichotomyList.h
+++ DichotomyList/DichotomyList.h
@@ -30,6 +30,8 @@ private:
     void QuickSort_Switch2(std::vector<int>&nums,int left,int right);
     /*交换*/
     void Swap(std::vector<int>&nums,int left,int right);
+    /*交换beta*/
+    void Swap_Beta(std::vector<int>&nums,int left,int right);
     /*快排递归1*/
     int QuickSort_BFS_1(std::vector<int>&nums,int left,int right);
     /*快排递归2 索引交换*/
@@ -71,4 +73,91 @@ private:
 public:
     /*希尔排序*/
     void Test_HearArrySort_();
+private:
+    /*最多重合的线段*/
+    int TheMostCoincidentSegments_(std::vector<std::vector<int>>&line);
+public:
+    /*最多重合的线段*/
+    void Test_TheMostCoincidentSegments();
+private:
+    /*给你一个正整数数组 nums 。每一次操作中，
+    你可以从 nums 中选择 任意 一个数并将它减小到 恰好 一半。
+    （注意，在后续操作中你可以对减半过的数继续执行操作）
+    请你返回将 nums 数组和 至少 减少一半的 最少 操作数。
+    示例 1：
+    输入：nums = [5,19,8,1]
+    输出：3
+    解释：初始 nums 的和为 5 + 19 + 8 + 1 = 33 。
+    以下是将数组和减少至少一半的一种方法：
+    选择数字 19 并减小为 9.5 。
+    选择数字 9.5 并减小为 4.75 。
+    选择数字 8 并减小为 4 。
+    最终数组为 [5, 4.75, 4, 1] ，和为 5 + 4.75 + 4 + 1 = 14.75 。
+    nums 的和减小了 33 - 14.75 = 18.25 ，
+    减小的部分超过了初始数组和的一半，18.25 >= 33/2 = 16.5 。
+    我们需要 3 个操作实现题目要求，所以返回 3 。
+    可以证明，无法通过少于 3 个操作使数组和减少至少一半。*/
+    int LC2208_halveArray_(std::vector<int>& nums);
+public:
+    /*LeetCode 2208 将数组和减半的最少操作次数*/
+    void Test_LC2208_halveArray();
+private:
+    /*不使用比较而判断两个值中的最大值*/
+    int GetMax(int a,int b);
+    /*取反*/
+    int flip(int sign);
+    /*移位,取符号*/
+    int sign(int num);
+public:
+    /*不使用比较而判断两个值中的最大值*/
+    void Test_GetMax();
+private:
+    /*给你一个整数数组 nums，其中恰好有两个元素只出现一次，
+    其余所有元素均出现两次。
+    找出只出现一次的那两个元素。你可以按 任意顺序 返回答案。
+    你必须设计并实现线性时间复杂度的算法且仅使用常量额外空间来解决此问题。
+    示例 1：
+    输入：nums = [1,2,1,3,2,5]
+    输出：[3,5]
+    解释：[5, 3] 也是有效的答案。
+    示例 2：
+    输入：nums = [-1,0]
+    输出：[-1,0]
+    示例 3：
+    输入：nums = [0,1]
+    输出：[1,0]*/
+    std::vector<int>LC260_singleNumber(std::vector<int>&nums);
+public:
+    /*LeetCode 260 只出现奇数次的数字*/
+    void Test_LC260_singleNumber();
+private:
+    /*判断一个数是否是2的幂*/
+    bool isPowerOfTwo(int n);
+public:
+    /*判断一个数是否是2的幂*/
+    void Test_isPowerOfTwo();
+private:
+    /*返回大于等于n的最小的2的某次方*/
+    int near2power(int n);
+public:
+    /*返回大于等于n的最小的2的某次方*/
+    void near2power();
+private:
+    /*颠倒给定的 32 位无符号整数的二进制位。*/
+    uint32_t LC190_reverseBits(uint32_t n);
+public:
+    /*LeetCode 190 颠倒二进制位*/
+    void Test_LC190_reverseBits();
+private:    
+    /*位图的对数器*/
+    bool BitSetComp(std::vector<int>&nums,int MaxNum);
+public:
+    /*LeetCode 2166 设计位集*/
+    void Test_BitSetComp();
+private:
+    /*LeetCode 29 位运算*/
+    void BitOperator_Cop(std::vector<int>num1,std::vector<int>num2);
+public:
+    /*位运算对数器*/
+    void Test_BitOperator_Cop();
 };
