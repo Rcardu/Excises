ad95aa150221e6dc69dc0538297ca0fbe5a18be6
ricardo <gq1090523745@163.com>
2023-09-09 21:45:25 +0800

    随机数生成与对数器测试

--- VerifyCleanup.h
+++ VerifyCleanup.h
@@ -15,6 +15,11 @@ private:
         arr[s1]=arr[s2];
         arr[s2]=temp;
     }
+    void Swap_(int s1,int s2,std::vector<double>&arr){
+        int temp=arr[s1];
+        arr[s1]=arr[s2];
+        arr[s2]=temp;
+    }
 public:
     //随机生成一个数组
     std::vector<int>RandVector(int len,int range1,int range2,int times){
@@ -25,6 +30,20 @@ public:
         for(int i=0;i<len;i++){
             arr[i]=u(e);
         }
+        if(len==1)return arr;
+        Swap_(0,RandNum(1,arr.size()-1,times),arr);
+        return arr;
+    }
+    //随机生成一个实数数组
+    std::vector<double>RandVector(int len,double range1,double range2,int times){
+        std::vector<double>arr(len);
+        std::default_random_engine e;
+        std::uniform_real_distribution<double> u(range1,range2);
+        e.seed(time(0)+times);
+        for(int i=0;i<len;i++){
+            arr[i]=u(e);
+        }
+        if(len==1)return arr;
         Swap_(0,RandNum(1,arr.size()-1,times),arr);
         return arr;
     }
@@ -39,6 +58,7 @@ public:
         std::uniform_int_distribution<int> u(range1,range2);
         std::unordered_set<int>set;
         e.seed(time(0)+times);
+        int n=u(e);
         for(int i=0;i<len;i++){
             int num=u(e);
             if(set.count(num)){
@@ -48,7 +68,8 @@ public:
             arr[i]=num;
             set.insert(num);
         }
-        Swap_(0,RandNum(range1,range2,times),arr);
+        if(len==1)return arr;
+        Swap_(0,RandNum(1,arr.size()-1,times),arr);
         
         return arr;
      }
@@ -56,6 +77,13 @@ public:
     int RandNum(int range1,int range2,int times){
         std::default_random_engine e;
         std::uniform_int_distribution<int> u(range1,range2);
+        e.seed(time(0)+times);
+        return u(e);
+    }
+    //随机生成一个实数
+    double RandRealNum(int range1,int range2,int times){
+        std::default_random_engine e;
+        std::uniform_real_distribution<double>u(range1,range2);
         e.seed(time(0)+times);
         return u(e);
     }
@@ -92,7 +120,6 @@ public:
         }
         return true;
     }
-
     //打印此数组
     void Print(std::vector<int>&arr){
         for(int a:arr)std::cout<<a<<"  ";
