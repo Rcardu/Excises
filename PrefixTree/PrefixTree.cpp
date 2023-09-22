0933c92c4d95e30baadaa3b6b5f8f9080cf32722
ricardo <gq1090523745@163.com>
2023-09-22 22:14:08 +0800

    前缀树

--- PrefixTree/PrefixTree.cpp
+++ PrefixTree/PrefixTree.cpp
@@ -210,10 +210,390 @@ void StaticTrie::Test_StaticTrie(){
     cout<<Search("app")<<endl;
 }
 
+/// @brief 累加和为给定值的最长子数组长度
+/// @param nums 
+/// @param aim 
+/// @return 
+int Prefix::LongestSubarraySumEqualsAim_(vector<int>nums,int aim){
+    unordered_map<int,int>map;
+    //0这个前缀和在一个数字也没有的时候就存在了
+    map[0]=-1;
+    int sum=0;
+    int ans=0;
+    for(int i=0;i<nums.size();i++){
+        sum+=nums[i];
+        if(map.count(sum-aim))ans=max(ans,i-map[sum-aim]);
+        if(!map.count(sum))map[sum]=i;
+    }
+    //累加和等于aim的最大长度
+    
+    return ans;
+}
+void Prefix::Test_LongestSubarraySumEqualsAim(){
+    vector<int>nums={2, 0, 3, -5, 2, -1};
+    cout<<LongestSubarraySumEqualsAim_(nums,0)<<endl;
+}
+
+/// @brief LeetCode 60 和为k的子数组
+/// @param nums 
+/// @param k 
+/// @return 
+int Prefix::LC560_subarraySum_(std::vector<int>&nums,int k){
+    unordered_map<int,int>map;
+    map[0]=1;
+    int sum=0;
+    int ans=0;
+    for(int i=0;i<nums.size();i++){
+        sum+=nums[i];
+        ans+=map[sum-k];
+        map[sum]++;
+    }
+    return ans;
+}
+void Prefix::Test_LC560_subarraySum(){
+    vector<int>nums={-1,-1,1};
+    cout<<LC560_subarraySum_(nums,0)<<endl;
+}
+
+/// @brief LeetCode 1124 表现良好的最长时间段
+/// @param hours 
+/// @return 
+int Prefix::LC1124_longestWPI_(vector<int>hours){
+    unordered_map<int,int>map;
+    map[0]=-1;
+    int sum=0;
+    int ans=0;
+    for(int i=0;i<hours.size();i++){
+        sum+=hours[i]>8?1:-1;
+        if(sum>0)ans=i+1;
+        else{
+            if(map.count(sum-1))ans=max(ans,i-map[sum-1]);
+        }
+        if(!map.count(sum))map[sum]=i;
+    }
+    return ans;
+}
+void Prefix::Test_LC1124_longestWPI(){
+    vector<int>nums;
+    LC1124_longestWPI_(nums);
+}
+
+/// @brief LeetCode 1590 可以被p整除的最小要删掉的子数组的长度
+/// @param nums 
+/// @param p 
+/// @return 
+int Prefix::LC1590_minSubarray_(vector<int>&nums,int p){
+    int mod=0;
+    for(int num:nums)mod=(mod+num)%p;
+    if(mod==0)return 0;
+    unordered_map<int,int>map;
+    map[0]=-1;
+    int ans=INT_MAX;
+    for(int i=0,cur=0,find;i<nums.size();i++){
+        cur=(cur+nums[i])%p;
+        //find=cur>=mod?cur-mod:(cur+p-mod);
+        find=(cur+p-mod)%p;
+        if(map.count(find))ans=min(ans,i-map[find]);
+        map[cur]=i;
+    }
+    return ans==nums.size()?-1:ans;
+}
+void Prefix::Test_LC1590_minSubarray(){
+    vector<int>nums;
+    LC1590_minSubarray_(nums,2);
+}
+
+
+int Prefix::LC1371_findTheLongestSubstring_(string s){
+    //用-2表示这个状态之前没出现过
+    vector<int>map(32,-2);
+    //0:00000
+    //天然是偶数状态
+    map[0]=-1;
+    int ans=0;
+    for(int i=0,status=0;i<s.size();i++){
+        //status:0....i字符上，aeiou的奇偶性
+        //s[i]=当前字符
+        //case 1:当前字符不是元音字符
+        //case 2:当前字符是元音
+        int m=LC1371_findTheLongestSubstring_move_(s[i]);
+        if(m!=-1){
+            status^=1<<m;
+        }
+        if(map[status]!=-2)ans=max(ans,i-map[status]);
+        else map[status]=i;
+    }
+    return ans;
+}
+int Prefix::LC1371_findTheLongestSubstring_move_(char a){
+    switch(a){
+        case 'a':return 0;
+        case 'e':return 1;
+        case 'i':return 2;
+        case 'o':return 3;
+        case 'u':return 4;
+        default:return -1;
+    }
+}
+void Prefix::Test_LC1371_findTheLongestSubstring(){
+    string s;
+    LC1371_findTheLongestSubstring_(s);
+}
+
+/// @brief 接头密钥
+namespace CountConsistenKeys{
+    extern vector<vector<int>>tree;
+    extern vector<int>pass;
+    extern vector<int>end;
+    extern int cnt;
+
+
+    void build(){cnt=1;}
+    void Insert(string word){
+        int cur=1;
+        pass[cur]++;
+        for(int i=0,path;i<word.size();i++){
+            path=Path(word[i]);
+            if(tree[cur][path]==0)tree[cur][path]=++cnt;
+            cur=tree[cur][path];
+            pass[cur]++;
+        }
+        end[cur]++;
+    }
+    int Serach(string word){
+        int cur=1;
+        for(int i=0,path;i<word.size();i++){
+            path=Path(word[i]);
+            if(tree[cur][path]==0)return 0;
+            cur=tree[cur][path];
+        }
+        return end[cur];
+    }
+    int PrefixNumber(string prefix){
+        int cur=1;
+        for(int i=0,path;i<prefix.size();i++){
+            path=Path(prefix[i]);
+            if(tree[cur][path]==0)return 0;
+            cur=tree[cur][path];
+        }
+        return pass[cur];
+    }
+    int Path(char ch){
+        if(ch-'#'==0)return 10;
+        else if(ch-'-'==0)return 11;
+        else return ch-'0';
+    }
+    void remove(string word){
+        if(Serach(word)<1)return;
+        int cur=1;
+        pass[cur]--;
+        for(int i=0,path;i<word.size();i++){
+            path=Path(word[i]);
+            if(--pass[tree[cur][path]]==0){
+                tree[cur][path]=0;
+                return;
+            }
+            cur=tree[cur][path];
+        }
+        end[cur]--;
+    }
+    void Clear(){
+        for(int i=0;i<=cnt;i++){
+            for(int j=0;j<tree[i].size();i++)tree[i][j]=0;
+            pass[i]=0;
+            end[i]=0;
+        }
+    }
+
+    vector<int> countConsistenKeys(vector<vector<int>>b,vector<vector<int>>a){
+        for(int i=0;i<b.size();i++){
+            string str;
+            for(int j=1;j<b[i].size();j++)str+=to_string(b[i][j]-b[i][j-1])+"#";
+            Insert(str);
+        }
+        vector<int>ans(a.size());
+        for(int i=0;i<a.size();i++){
+            string str;
+            for(int j=1;j<a[i].size();j++)str+=to_string(a[i][j]-a[i][j-1])+"#";
+            ans[i]=PrefixNumber(str);
+        }
+        Clear();
+        return ans;
+    }
+    void Test_CountConsistenKeys(){
+        Cleanup cle;
+        vector<vector<int>>b={{3,6,12,20},{1,9},{4,7,2},{1,4,-1}};
+        vector<vector<int>>a={{2,5},{7,10,5}};
+        /*
+        for(int i=0;i<10;i++){
+            vector<int>nums=cle.RandVector(6,-15,10,i);
+            cle.Print(nums);
+            if(i%2==0)b.push_back(nums);
+            else a.push_back(nums);
+        }
+        */
+        build();
+        countConsistenKeys(b,a);
+    }
+}
+
+namespace LC421{
+    void build(vector<int>nums){
+        cnt=1;
+        int Max=INT_MIN;
+        for(int num:nums)Max=max(Max,num);
+        //计算最大值的前导0个数，可以忽略这些0，从left开始考虑
+        left=32-__builtin_clz(Max);
+        for(int num:nums)Insert(num);
+    }
+    void Insert(int num){
+        int cur=1;
+        for(int i=left,path;i>=0;i--){
+            path=(num>>i)&1;
+            if(tree[cur][path]==0)tree[cur][path]=++cnt;
+            cur=tree[cur][path];
+        }
+    }
+    int maxXor(int num){
+        int ans=0;
+        int cur=1;
+        for(int i=left,status,want;i>=0;i--){
+            status=(num>>i)&1;
+            want=status^1;
+            if(tree[cur][want]==0)want^=1;
+            //如果两个状态相等那么说明没找到想要的状态，直接或进ans
+            ans|=(status^want)<<i;
+            cur=tree[cur][want];
+        }
+        return ans;
+    }
+    void Clear(){
+        for(int i=0;i<=cnt;i++){
+            tree[i][0]=0;
+            tree[i][1]=0;
+        }
+    }
+
+    int LC421_findMaximumXOR(std::vector<int>&nums){
+        build(nums);
+        int ans=0;
+        for(int num:nums)ans=max(ans,maxXor(num));
+        Clear();
+        return ans;
+    }
+    int LC421_findMaximumXOR_Hash(std::vector<int>nums){
+        //从最高位为1的地方开始建立ans
+        int Max=INT_MIN;
+        for(int num:nums)Max=max(Max,num);
+        int ans=0;
+        unordered_set<int>set;
+        for(int i=Max==0?0:(32-__builtin_clz(Max));i>=0;i--){
+            //ans就是在保持32……i+1位已生成的情况下
+            //第i位能否取到1
+            //better：表示第32……i位希望取到的数，也就是第i位希望取到1
+            int better=ans|(1<<i);
+            set.clear();
+            for(int num:nums){
+                //每次遍历整个num在考虑第32……第i位，但不考虑第i-1位到第0位的情况下需要取得的数
+                num=(num>>i)<<i;
+                set.insert(num);
+                /*如果存在一个num' 使得num'^num的值就是better
+                那么这个值num'^num=better等价于ans^num=num'
+                我们希望在遍历的过程中能在set中找到这个值即，num'*/
+                if(set.count(better^ans)){
+                    //那么ans就可以成为better
+                    ans=better;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+    void Test_LC421_findMaximumXOR(){
+        vector<int>nums={1};
+        cout<<LC421_findMaximumXOR_Hash(nums)<<endl;
+    }
+}
+
+
+namespace LC212{
+    void build(vector<string>words){
+        cnt=1;
+        for(string str:words)Insert(str);
+    }
+    void Insert(string word){
+        int cur=1;
+        pass[cur]++;
+        for(int i=0,path;i<word.size();i++){
+            path=word[i]-'a';
+            if(tree[cur][path]==0)tree[cur][path]=++cnt;
+            cur=tree[cur][path];
+            pass[cur]++;
+        }
+        end[cur]=word;
+    }
+    void Clear(){
+        for(int i=0;i<=cnt;i++){
+            for(int j=0;j<26;j++)tree[i][j]=0;
+            end[i]="";
+            pass[i]=0;
+        }
+    }
+
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words){
+        build(words);
+        vector<string>ans;
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[i].size();j++){
+                dfs(board,i,j,1,ans);
+            }
+        }
+        return ans;
+    }
+    //(i,j)当前所在网格
+    //t前缀树的编号
+    int dfs(vector<vector<char>>&board,int i,int j,int t,vector<string>&ans){
+        //越界或者重复走了路
+        if(i<0||i==board.size()||j<0||j<board[0].size()||board[i][j]==0)return 0;
+        char tmp=board[i][j];
+        int road=tmp-'a';
+        t=tree[t][road];
+        if(t==0||pass[t]==0)return 0;
+        //从当前位置出发一共收集到几个字符串
+        int fix=0;
+        if(end[t]!=""){
+            fix++;
+            ans.push_back(end[t]);
+            end[t]="";
+        }
+        board[i][j]=0;
+        fix+=dfs(board,i-1,j,t,ans);
+        fix+=dfs(board,i+1,j,t,ans);
+        fix+=dfs(board,i,j-1,t,ans);
+        fix+=dfs(board,i,j+1,t,ans);
+        pass[t]-=fix;
+        board[i][j]=tmp;
+        return fix;
+    }
+}
+
+
 int main(int argc,char*argv[]){
     auto start=chrono::system_clock::now();
-    StaticTrie tr;
-    tr.Test_StaticTrie();
+
+    using LC421::Test_LC421_findMaximumXOR;
+    Test_LC421_findMaximumXOR();
+    //cout<<__builtin_clz(7)<<endl;
+
+
+    //using CountConsistenKeys::Test_CountConsistenKeys;
+    //Test_CountConsistenKeys();
+
+    //Prefix pre;
+    //pre.Test_LC560_subarraySum();
+
+    //StaticTrie tr;
+    //tr.Test_StaticTrie();
     //getchar();
     //char ch='a';
 
--- PrefixTree/PrefixTree.h
+++ PrefixTree/PrefixTree.h
@@ -107,3 +107,175 @@ public:
 public:
     void Test_StaticTrie();
 };
+class Prefix{
+private:
+    /*构建前缀和最早出现的位置。返回无序数组中累加和为给定值的最长子数组长度*/
+    int LongestSubarraySumEqualsAim_(std::vector<int>nums,int aim);
+public:
+    /*累加和为给定值的最长子数组长度*/
+    void Test_LongestSubarraySumEqualsAim();
+private:
+    /*给你一个整数数组 nums 和一个整数 k ，
+    请你统计并返回 该数组中和为 k 的连续子数组的个数 。
+    子数组是数组中元素的连续非空序列。
+    示例 1：
+    输入：nums = [1,1,1], k = 2
+    输出：2
+    示例 2：
+    输入：nums = [1,2,3], k = 3
+    输出：2*/
+    int LC560_subarraySum_(std::vector<int>&nums,int k);
+public:
+    /*LeetCode 60 和为k的子数组*/
+    void Test_LC560_subarraySum();
+private:
+    /*给你一份工作时间表 hours，上面记录着某一位员工每天的工作小时数。
+    我们认为当员工一天中的工作小时数大于 8 小时的时候，
+    那么这一天就是「劳累的一天」。
+    所谓「表现良好的时间段」，意味在这段时间内，
+    「劳累的天数」是严格 大于「不劳累的天数」。
+    请你返回「表现良好时间段」的最大长度。
+    示例 1：
+    输入：hours = [9,9,6,0,6,6,9]
+    输出：3
+    解释：最长的表现良好时间段是 [9,9,6]。
+    示例 2：
+    输入：hours = [6,6,6]
+    输出：0*/
+    int LC1124_longestWPI_(std::vector<int>hours);
+public:
+    /*LeetCode 1124 表现良好的最长时间段*/
+    void Test_LC1124_longestWPI();
+private:
+    /*给你一个正整数数组 nums，请你移除 最短 子数组（可以为 空），
+    使得剩余元素的 和 能被 p 整除。 不允许 将整个数组都移除。
+    请你返回你需要移除的最短子数组的长度，如果无法满足题目要求，返回 -1 。
+    子数组 定义为原数组中连续的一组元素。
+    示例 1：
+    输入：nums = [3,1,4,2], p = 6
+    输出：1
+    解释：nums 中元素和为 10，不能被 p 整除。我们可以移除子数组 [4] ，剩余元素的和为 6 。
+    示例 2：
+    输入：nums = [6,3,5,2], p = 9
+    输出：2
+    解释：我们无法移除任何一个元素使得和被 9 整除，最优方案是移除子数组 [5,2] ，剩余元素为 [6,3]，和为 9 。
+    示例 3：
+    输入：nums = [1,2,3], p = 3
+    输出：0
+    解释：和恰好为 6 ，已经能被 3 整除了。所以我们不需要移除任何元素。
+    示例  4：
+    输入：nums = [1,2,3], p = 7
+    输出：-1
+    解释：没有任何方案使得移除子数组后剩余元素的和被 7 整除。
+    示例 5：
+    输入：nums = [1000000000,1000000000,1000000000], p = 3
+    输出：0*/
+    int LC1590_minSubarray_(std::vector<int>&nums,int p);
+public:
+    /*LeetCode 1590 可以被p整除的最小要删掉的子数组的长度*/
+    void Test_LC1590_minSubarray();
+private:
+    /*给你一个字符串 s ，
+    请你返回满足以下条件的最长子字符串的长度：每个元音字母，
+    即 'a'，'e'，'i'，'o'，'u' ，在子字符串中都恰好出现了偶数次。
+    示例 1：
+    输入：s = "eleetminicoworoep"
+    输出：13
+    解释：最长子字符串是 "leetminicowor" ，
+    它包含 e，i，o 各 2 个，以及 0 个 a，u 。
+    示例 2：
+    输入：s = "leetcodeisgreat"
+    输出：5
+    解释：最长子字符串是 "leetc" ，其中包含 2 个 e 。
+    示例 3：
+    输入：s = "bcbcbc"
+    输出：6
+    解释：这个示例中，字符串 "bcbcbc" 本身就是最长的，
+    因为所有的元音 a，e，i，o，u 都出现了 0 次。
+    提示：
+    1 <= s.length <= 5 x 10^5
+    s 只包含小写英文字母。*/
+    int LC1371_findTheLongestSubstring_(std::string s);
+    /*元音的提取*/
+    int LC1371_findTheLongestSubstring_move_(char a);
+public:
+    void Test_LC1371_findTheLongestSubstring();
+};
+
+namespace CountConsistenKeys{
+using std::vector;
+    static int MAXN=150001;
+    vector<vector<int>>tree(MAXN,vector<int>(12,0));
+    vector<int>pass(MAXN,0);
+    vector<int>end(MAXN,0);
+    int cnt;
+    void build();
+    void Insert(std::string word);
+    int Serach(std::string word);
+    int PrefixNumber(std::string prefix);
+    int Path(char ch);
+    void remove(std::string word);
+    void Clear();
+    /*接头密钥
+    牛牛和他的朋友们约定了一套接头密钥系统用于识别彼此身份
+    密钥由一组数字序列表示，两个密钥被认定为是一致的，如果满足以下条件
+    密钥b的长度不会超过密钥a
+    对于任意的0<=i<length(b),有b[i+1]-b[i]==a[i+1]-a[i]
+    现在给定了m个密钥b的数组，以及n个密钥a的数组
+    请你返回一个长度为m的结果数组ans表示每个密钥b都有多少一致的密钥
+    数组a和数组b中的元素个数均不超过10^5
+    1<=m,n<=1000*/
+    /*前缀树方法时间：O( a数组的数字个数*10)+O(b数组的数字个数*10)
+    空间：O(a数组的数字个数*10 )*/
+    vector<int> countConsistenKeys(std::vector<std::vector<int>>b,std::vector<std::vector<int>>a);
+
+    void Test_CountConsistenKeys();
+}
+namespace LC421{
+    using std::vector;
+    using std::string;
+    static int MAXN=150001;
+    vector<vector<int>>tree(MAXN,vector<int>(2,0));
+    int cnt;
+    int left;
+
+    void build(vector<int>nums);
+    void Insert(int num);
+    int maxXor(int num);
+    void Clear();
+
+    /*给你一个整数数组 nums ，返回 nums[i] XOR nums[j] 的最大运算结果，
+    其中 0 ≤ i ≤ j < n 。
+    示例 1：
+    输入：nums = [3,10,5,25,2,8]
+    输出：28
+    解释：最大运算结果是 5 XOR 25 = 28.
+    示例 2：
+    输入：nums = [14,70,53,83,49,91,36,80,92,51,66,70]
+    输出：127*/
+    int LC421_findMaximumXOR(std::vector<int>&nums);
+    //哈希表的实现
+    int LC421_findMaximumXOR_Hash(std::vector<int>nums);
+    void Test_LC421_findMaximumXOR();
+}
+    /*给定一个 m x n 二维字符网格 board 和一个单词（字符串）列表 words，
+    返回所有二维网格上的单词 。
+    单词必须按照字母顺序，通过 相邻的单元格 内的字母构成，
+    其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+    同一个单元格内的字母在一个单词中不允许被重复使用。*/
+namespace LC212{
+    static int MAXN=150001;
+    using std::vector;
+    using std::string;
+    vector<vector<int>>tree(MAXN,vector<int>(26,0));
+    vector<int>pass(MAXN);
+    vector<string>end(MAXN,"");
+    int cnt;
+    void build(vector<string>words);
+    void Insert(string word);
+    void Clear();
+
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words);
+    int dfs(vector<vector<char>>&board,int i,int j,int t,vector<string>&ans);
+}
+
